30471461f45fa1fd9f514d019458fd3c6b3183dd
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-10-11 18:24:31 +0900

    prototypeの修正２

--- db/migrate/20201005102417_create_prototypes.rb
+++ db/migrate/20201005102417_create_prototypes.rb
@@ -4,7 +4,6 @@ class CreatePrototypes < ActiveRecord::Migration[6.0]
       t.string :title,      null: false
       t.text :catch_copy,     null: false
       t.text :concept,      null: false
-      t.text :image,     null: false
       t.references :user, foreign_key: true #user_idというカラムの追加
       t.timestamps
     end
