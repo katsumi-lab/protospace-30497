0583820d4c293f82d7202b9ad2cf71bb1cbb58b7
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-10-12 20:01:46 +0900

    prototype修正５

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -22,7 +22,7 @@ class PrototypesController < ApplicationController
 
   def show  
     @comment = Comment.new
-    @comments = @prototype.comments.includes(:user)
+    @comments = Comment.where(prototype_id: @prototype.id)
   end
 
   def edit
