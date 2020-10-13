310d72f731311f262b7f846c20163c4d474c854b
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-10-13 21:39:56 +0900

    prototypeの修正７

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,8 @@
 class UsersController < ApplicationController
   def show
+    #Pathパラメーターで送信されるID値Userモデルの特定のオブジェクトを取得
     @user = User.find(params[:id])
-    # @user = user(user_params)
-    @prototypes = current_user.prototypes
-  end
-
-  private
-
-  def user_params
-    params.permit(:id,:email, :name, :profile, :occupation,:position)
+    #UserとPrototypeのアソシエーションでPrototypeが主役
+    @prototypes = @user.prototypes
   end
 end
